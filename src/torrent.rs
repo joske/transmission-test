4250357541b8beb6b16c15cc847051406704ce37
Jos Dehaes <jos.dehaes@gmail.com>
2025-12-04 13:01:15 +0100

    initial

---
+++ src/torrent.rs
@@ -0,0 +1,107 @@
+use bip_metainfo::{MetainfoBuilder, PieceLength};
+
+#[allow(dead_code)]
+/// Create a torrent file from a folder.
+fn create_torrent_file(folder: &str, output_file: &str) {
+    let builder = MetainfoBuilder::new()
+        .set_piece_length(PieceLength::OptBalanced)
+        .set_main_tracker(Some("udp://tracker.opentrackr.org:1337/announce"));
+    let bytes = builder.build(1, folder, |_| {}).unwrap();
+    std::fs::write(output_file, bytes).unwrap();
+}
+
+#[allow(dead_code)]
+pub trait BitTorrent {
+    /// Add a torrent file to Transmission. The torrents starts downloading/seeding immediately.
+    async fn add(&self, torrent_file: &str) -> Torrent;
+    /// Stop torrents by their IDs. The IDs should be the torrent hash.
+    async fn stop(&self, ids: Vec<String>);
+    /// List all torrents.
+    async fn list(&self) -> Vec<Torrent>;
+    /// Remove torrents by their IDs (torrent hash). If `delete_local_data` is true, the local data will also be deleted.
+    async fn remove(&self, ids: Vec<String>, delete_local_data: bool);
+    /// Get session statistics.
+    async fn stats(&self) -> SessionStats;
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct SessionStats {
+    pub active_torrent_count: i32,
+    pub cumulative_stats: StatsDetails,
+    pub current_stats: StatsDetails,
+    pub download_speed: i32,
+    pub paused_torrent_count: i32,
+    pub torrent_count: i32,
+    pub upload_speed: i32,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct StatsDetails {
+    pub downloaded_bytes: i64,
+    pub files_added: i64,
+    pub seconds_active: i64,
+    pub session_count: i64,
+    pub uploaded_bytes: i64,
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Torrent {
+    pub id: i32,
+    pub activity_date: i32,
+    pub added_date: i32,
+    pub bandwidth_priority: i32,
+    pub comment: String,
+    pub corrupt_ever: i64,
+    pub creator: String,
+    pub date_created: i32,
+    pub desired_available: i64,
+    pub done_date: i32,
+    pub download_dir: String,
+    pub download_limit: i32,
+    pub download_limited: bool,
+    pub downloaded_ever: i64,
+    pub edit_date: i32,
+    pub error: i32,
+    pub error_string: String,
+    pub eta: i64,
+    pub eta_idle: i64,
+    pub hash_string: String,
+    pub have_unchecked: i64,
+    pub have_valid: i64,
+    pub honors_session_limits: bool,
+    pub is_finished: bool,
+    pub is_private: bool,
+    pub is_stalled: bool,
+    pub left_until_done: i64,
+    pub magnet_link: String,
+    pub manual_announce_time: i32,
+    pub metadata_percent_complete: f32,
+    pub name: String,
+    pub percent_done: f32,
+    pub piece_count: i64,
+    pub piece_size: i64,
+    pub pieces: String,
+    pub primary_mime_type: String,
+    pub queue_position: i32,
+    pub rate_download: i32,
+    pub rate_upload: i32,
+    pub recheck_progress: f32,
+    pub seconds_downloading: i32,
+    pub seconds_seeding: i32,
+    pub seed_idle_limit: i32,
+    pub seed_idle_mode: i32,
+    pub seed_ratio_limit: f32,
+    pub seed_ratio_mode: i32,
+    pub size_when_done: i64,
+    pub start_date: i32,
+    pub status: i32,
+    pub torrent_file: String,
+    pub total_size: i64,
+    pub upload_limit: i32,
+    pub upload_limited: bool,
+    pub upload_ratio: f32,
+    pub uploaded_ever: i64,
+}
---
+++ src/transmission.rs
@@ -0,0 +1,145 @@
+use transmission_client::{
+    Client, SessionStats as TransmissionSessionStats, StatsDetails as TransmissionStatsDetails,
+    Torrent as TransmissionTorrent,
+};
+use url::Url;
+
+use crate::torrent::{BitTorrent, SessionStats, StatsDetails, Torrent};
+
+pub struct TransmissionClient {
+    client: Client,
+}
+
+impl TransmissionClient {
+    pub fn new(url: Option<&str>) -> Self {
+        let url = Url::parse(url.unwrap_or("http://localhost:9091/transmission/rpc")).unwrap();
+        let client = Client::new(url);
+        Self { client }
+    }
+}
+
+impl BitTorrent for TransmissionClient {
+    async fn add(&self, torrent_file: &str) -> Torrent {
+        self.client
+            .torrent_add_filename(torrent_file)
+            .await
+            .unwrap()
+            .unwrap()
+            .into()
+    }
+
+    async fn stop(&self, ids: Vec<String>) {
+        self.client.torrent_stop(Some(ids)).await.unwrap();
+    }
+
+    async fn list(&self) -> Vec<Torrent> {
+        self.client
+            .torrents(None)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(Into::into)
+            .collect()
+    }
+
+    async fn remove(&self, ids: Vec<String>, delete_local_data: bool) {
+        self.client
+            .torrent_remove(Some(ids), delete_local_data)
+            .await
+            .unwrap();
+    }
+
+    async fn stats(&self) -> SessionStats {
+        self.client.session_stats().await.unwrap().into()
+    }
+}
+
+// conversions from transmission_client types to our defined types
+
+impl From<TransmissionSessionStats> for SessionStats {
+    fn from(value: TransmissionSessionStats) -> Self {
+        Self {
+            active_torrent_count: value.active_torrent_count,
+            cumulative_stats: value.cumulative_stats.into(),
+            current_stats: value.current_stats.into(),
+            download_speed: value.download_speed,
+            paused_torrent_count: value.paused_torrent_count,
+            torrent_count: value.torrent_count,
+            upload_speed: value.upload_speed,
+        }
+    }
+}
+
+impl From<TransmissionStatsDetails> for StatsDetails {
+    fn from(value: TransmissionStatsDetails) -> Self {
+        Self {
+            downloaded_bytes: value.downloaded_bytes,
+            files_added: value.files_added,
+            seconds_active: value.seconds_active,
+            session_count: value.session_count,
+            uploaded_bytes: value.uploaded_bytes,
+        }
+    }
+}
+
+impl From<TransmissionTorrent> for Torrent {
+    fn from(value: TransmissionTorrent) -> Self {
+        Self {
+            id: value.id,
+            activity_date: value.activity_date,
+            added_date: value.added_date,
+            bandwidth_priority: value.bandwidth_priority,
+            comment: value.comment,
+            corrupt_ever: value.corrupt_ever,
+            creator: value.creator,
+            date_created: value.date_created,
+            desired_available: value.desired_available,
+            done_date: value.done_date,
+            download_dir: value.download_dir,
+            download_limit: value.download_limit,
+            download_limited: value.download_limited,
+            downloaded_ever: value.downloaded_ever,
+            edit_date: value.edit_date,
+            error: value.error,
+            error_string: value.error_string,
+            eta: value.eta,
+            eta_idle: value.eta_idle,
+            hash_string: value.hash_string,
+            have_unchecked: value.have_unchecked,
+            have_valid: value.have_valid,
+            honors_session_limits: value.honors_session_limits,
+            is_finished: value.is_finished,
+            is_private: value.is_private,
+            is_stalled: value.is_stalled,
+            left_until_done: value.left_until_done,
+            magnet_link: value.magnet_link,
+            manual_announce_time: value.manual_announce_time,
+            metadata_percent_complete: value.metadata_percent_complete,
+            name: value.name,
+            percent_done: value.percent_done,
+            piece_count: value.piece_count,
+            piece_size: value.piece_size,
+            pieces: value.pieces,
+            primary_mime_type: value.primary_mime_type,
+            queue_position: value.queue_position,
+            rate_download: value.rate_download,
+            rate_upload: value.rate_upload,
+            recheck_progress: value.recheck_progress,
+            seconds_downloading: value.seconds_downloading,
+            seconds_seeding: value.seconds_seeding,
+            seed_idle_limit: value.seed_idle_limit,
+            seed_idle_mode: value.seed_idle_mode,
+            seed_ratio_limit: value.seed_ratio_limit,
+            seed_ratio_mode: value.seed_ratio_mode,
+            size_when_done: value.size_when_done,
+            start_date: value.start_date,
+            status: value.status,
+            torrent_file: value.torrent_file,
+            total_size: value.total_size,
+            upload_limit: value.upload_limit,
+            upload_limited: value.upload_limited,
+            upload_ratio: value.upload_ratio,
+            uploaded_ever: value.uploaded_ever,
+        }
+    }
+}
